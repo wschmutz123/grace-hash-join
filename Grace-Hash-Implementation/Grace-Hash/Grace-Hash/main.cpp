0ea7a8f589b6a054893bdf87c480288ab7e84e96
wschmutz <wschmutz@umich.edu>
2024-10-21 12:30:54 -0700

    initial commit

---
+++ Grace-Hash-Implementation/Grace-Hash/Grace-Hash/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project-4-EECS-484
+//
+//  Created by Will Schmutz on 4/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
